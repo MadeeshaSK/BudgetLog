8ee0bb7c8939de750f95a7bec7a535e3709f0750
MadeeshaSK <madeeshasachindu2@gmail.com>
2025-01-01 22:41:58 +0530

    initialize

---
+++ src/main/java/me/madeeshask/budgetlog/BudgetLog.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.madeeshask.budgetlog;
+
+/**
+ *
+ * @author Madeesha
+ */
+public class BudgetLog {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
