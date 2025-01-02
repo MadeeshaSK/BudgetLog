0b398fc553736f392356bcc73e9e3bfb64cc3d8f
MadeeshaSK <madeeshasachindu2@gmail.com>
2025-01-02 12:40:15 +0530

    create main class and check connectivity

--- src/main/java/me/madeeshask/budgetlog/Main.java
+++ src/main/java/me/madeeshask/budgetlog/Main.java
@@ -2,10 +2,15 @@
 
 package me.madeeshask.budgetlog;
 
+import me.madeeshask.budgetlog.views.Login;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true); 
+            }
+        });
     }
 }
- 
