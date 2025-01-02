8260262350bc31d3c6bdaa87d19b1c9fc84e3950
MadeeshaSK <madeeshasachindu2@gmail.com>
2025-01-02 22:03:38 +0530

    update jframe

--- src/main/java/me/madeeshask/budgetlog/views/Login.java
+++ src/main/java/me/madeeshask/budgetlog/views/Login.java
@@ -8,7 +8,7 @@ public class Login extends javax.swing.JFrame {
     public Login() {
         initComponents();
         conScaleImage();
-        conSetWindowProperties();
+        conSetWindowProperties(); 
         
     }
     
