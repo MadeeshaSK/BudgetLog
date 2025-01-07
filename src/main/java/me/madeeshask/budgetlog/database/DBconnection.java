3e1ccd01a28911d8dee045e41e18bccd08be7430
MadeeshaSK <madeeshasachindu2@gmail.com>
2025-01-07 22:36:21 +0530

    updated default database

--- src/main/java/me/madeeshask/budgetlog/database/DBconnection.java
+++ src/main/java/me/madeeshask/budgetlog/database/DBconnection.java
@@ -13,19 +13,32 @@ public class DBconnection {
         try {
             String userHome = System.getProperty("user.home");
             String appFolder = userHome + File.separator + ".budgetlog";
+            String dbPath = appFolder + File.separator + "BudgetLog.db";
             File appDir = new File(appFolder);
             
             if (!appDir.exists()) {
                 appDir.mkdirs();
             }
             
-            String dbPath = DBconnection.class.getClassLoader().getResource("BudgetLog.db").getPath();
+            File dbFile = new File(dbPath);
+            if (!dbFile.exists()) {
+                try (InputStream is = DBconnection.class.getClassLoader().getResourceAsStream("BudgetLog.db");
+                     OutputStream os = new FileOutputStream(dbFile)) {
+                    if (is == null) {
+                        throw new IOException("Could not find BudgetLog.db in resources");
+                    }
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = is.read(buffer)) > 0) {
+                        os.write(buffer, 0, length);
+                    }
+                }
+            }
             
             conn = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
             System.out.println("Connection to SQLite has been established at: " + dbPath);
             
-                        
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             System.out.println("Database connection error: " + e.getMessage());
             e.printStackTrace();
         }
